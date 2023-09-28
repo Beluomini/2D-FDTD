552b1051791f99c586b510b1426d43d0e6f14781
Beluomini <lucasbeluomini1@gmail.com>
2023-09-28 14:30:18 -0300

    plan b é plan a e n ta dando erro

--- fdtd-2d/fdtd_mpi.c
+++ fdtd-2d/fdtd_mpi.c
@@ -10,7 +10,7 @@
 #include "fdtd-2d.h"
 
 // matrices and array initialization
-static void init_array (int tmax, int nx, int ny, double** ex, double** ey, double** hz, double* _fict_) {
+static void init_array (int tmax, int nx, int ny, double* ex, double* ey, double* hz, double* _fict_) {
   int i, j;
 
   for (i = 0; i < tmax; i++)
@@ -19,31 +19,25 @@ static void init_array (int tmax, int nx, int ny, double** ex, double** ey, doub
   for (i = 0; i < nx; i++)
     for (j = 0; j < ny; j++)
       {
-        ex[i][j] = ((double) i*(j+1)) / nx;
-        ey[i][j] = ((double) i*(j+2)) / ny;
-        hz[i][j] = ((double) i*(j+3)) / nx;
+        ex[i*nx+j] = ((double) i*(j+1)) / nx;
+        ey[i*nx+j] = ((double) i*(j+2)) / ny;
+        hz[i*nx+j] = ((double) i*(j+3)) / nx;
       }
 }
 
-void print_array(double** array, int x, int y){
+void print_array(double* array, int x, int y){
     for (int i=0;i<x;i++){
         for (int j=0;j<y;j++){
-        printf("%.2f ", array[i][j]);
+        printf("%.2f ", array[i*x+j]);
         }
         printf("\n");
     }
 }
 
 //função para alocar a matriz
-void* matrix_alloc_data(int x, int y){
+double* matrix_alloc_data(int x, int y){
 
-    // aloca um vetor de LIN ponteiros para linhas
-    double** vec = malloc (NX * sizeof (double*)) ;
-
-    // aloca cada uma das linhas (vetores de COL inteiros)
-    for (int i=0; i < NX; i++)
-        vec[i] = malloc (NY * sizeof (double)) ;
-
+  double* vec = (double*)malloc(x * y * sizeof(double));
   return vec;
 }
 
@@ -56,10 +50,7 @@ void* vector_alloc_data(int x){
   return vec;
 }
 
-void free_matrix(double** mat, int x){
-  for (int i=0;i<x;i++){
-    free(mat[i]);
-  }
+void free_matrix(double* mat, int x){
   free(mat);
 }
 
@@ -83,16 +74,16 @@ int main(int argc, char** argv) {
       if (!strcmp(argv[1],"-d")){
         if (!strcmp(argv[2],"small")){
           TMAX = 1;
-          NX = 6;
-          NY = 6;
+          NX = 8;
+          NY = 8;
         } else if (!strcmp(argv[2],"medium")){
           TMAX = 1;
-          NX = 6;
-          NY = 6;
+          NX = 8;
+          NY = 8;
         } else if (!strcmp(argv[2],"large")){
           TMAX = 1;
-          NX = 6;
-          NY = 6;
+          NX = 8;
+          NY = 8;
         } else {
           printf("Invalid dataset\n");
           return 0;
@@ -128,13 +119,19 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
 
-    double** ex = matrix_alloc_data(NX, NY);
-    double** ey = matrix_alloc_data(NX, NY);
-    double** hz = matrix_alloc_data(NX, NY);
-    double* _fict_ = vector_alloc_data(NY);
+    double* ex;
+    double* ey;
+    double* hz;
+    double* _fict_;
 
     if(process_Rank == 0){
 
+      ex = matrix_alloc_data(NX, NY);
+      ey = matrix_alloc_data(NX, NY);
+      hz = matrix_alloc_data(NX, NY);
+      _fict_ = vector_alloc_data(NY);
+
+      // Inicializa as matrizes e o vetor
       init_array (tmax, nx, ny, ex, ey, hz, _fict_);
 
       print_array(ey, NX, NY);
@@ -142,216 +139,60 @@ int main(int argc, char** argv) {
       print_array(ex, NX, NY);
       printf("\n---------------------\n");
       print_array(hz, NX, NY);
-      printf("\nsize of cluster: %d\n", size_Of_Cluster);
+      printf("\nsize of cluster: %d\n\n", size_Of_Cluster);
 
       // time function from polybench 
       //polybench_start_instruments;
-
-      for(int t = 0; t < tmax; t++){
-        
-        int start = 1;
-
-        int end = NY/(size_Of_Cluster/2);
-
-        for(int id = 1; id < size_Of_Cluster; id++){
-
-          printf("id: %d\n", id);
-
-          if(id < size_Of_Cluster/2){
-
-            int start = NY/(size_Of_Cluster/2) * id;
-
-            int end = NY/(size_Of_Cluster/2) * (id+1);
-                
-            MPI_Send(&start, 1, MPI_INT, id, 1, MPI_COMM_WORLD);
-            printf("Message Sent from %d to %d with start %d\n", 0, id, start);
-
-            MPI_Send(&end, 1, MPI_INT, id, 1, MPI_COMM_WORLD);
-            printf("Message Sent from %d to %d with end %d\n", 0, id, end);
-
-            MPI_Send(&ey[start][0], (end-start) * NX, MPI_INT, id, 1, MPI_COMM_WORLD);
-            printf("Message Sent from %d to %d with end %f\n", 0, id, ey[start][0]);
-            printf("end - start = %d\n", (end-start)*NX);
-
-            MPI_Send(&hz[start][0], (end-start) * NX, MPI_INT, id, 1, MPI_COMM_WORLD);
-            printf("Message Sent from %d to %d with end %f\n", 0, id, hz[start][0]);
-
-          }else{
-
-            int start = NY/(size_Of_Cluster/2) * (id-(size_Of_Cluster/2));
-
-            int end = NY/(size_Of_Cluster/2) * ((id-(size_Of_Cluster/2))+1);
-                
-            MPI_Send(&start, 1, MPI_INT, id, 1, MPI_COMM_WORLD);
-            printf("Message Sent from %d to %d with start %d\n", 0, id, start);
-
-            MPI_Send(&end, 1, MPI_INT, id, 1, MPI_COMM_WORLD);
-            printf("Message Sent from %d to %d with end %d\n", 0, id, end);
-
-            MPI_Send(&ex[start][0], (end-start) * NX, MPI_INT, id, 1, MPI_COMM_WORLD);
-            printf("Message Sent from %d to %d with end %f\n", 0, id, ex[start][0]);
-            printf("end - start = %d\n", (end-start)*NX);
-
-            MPI_Send(&hz[start][0], (end-start) * NX, MPI_INT, id, 1, MPI_COMM_WORLD);
-            printf("Message Sent from %d to %d with end %f\n", 0, id, hz[start][0]);
-
-          }
-
-        }
-
-        for (int f = 0; f < NY; f++)
-          ey[0][f] = _fict_[t];
-
-        for (int i=start ; i<end ; i++){
-          for (int j=0 ; j<NY ; j++){
-            ey[i][j] = ey[i][j] - 0.5*(hz[i][j]-hz[i-1][j]);
-          }
-        }
-
-        for (int i = 1; i < size_Of_Cluster; i++){
-
-          if(i < size_Of_Cluster/2){
-
-          
-            MPI_Recv(&start, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Message Received from %d to %d: %d\n", i, 0, start);
-
-            MPI_Recv(&end, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Message Received from %d to %d: %d\n", i, 0, end);
-
-            MPI_Recv(&ey[start][0], (end-start) * NX, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Message Received from %d to %d: %f\n", i, 0, ey[start][0]);
-
-            MPI_Recv(&hz[start][0], (end-start) * NX, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Message Received from %d to %d: %f\n", i, 0, hz[start][0]);            
-
-          }else{
-
-            MPI_Recv(&start, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Message Received from %d to %d: %d\n", i, 0, start);
-
-            MPI_Recv(&end, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Message Received from %d to %d: %d\n", i, 0, end);
-
-            MPI_Recv(&ex[start][0], (end-start) * NX, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Message Received from %d to %d: %f\n", i, 0, ex[start][0]);
-
-            MPI_Recv(&hz[start][0], (end-start) * NX, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Message Received from %d to %d: %f\n", i, 0, hz[start][0]);
-
-          }
-
-        }
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        print_array(ey, NX, NY);
-        printf("\n---------------------\n");
-        print_array(ex, NX, NY);
-        printf("\n---------------------\n");
-        print_array(hz, NX, NY);
-        printf("\nsize of cluster: %d\n", size_Of_Cluster);
-
-      }
 
       //stop timer and print results
       //polybench_stop_instruments;
       //polybench_print_instruments;
 
-      // free matrixes and array
-      free_matrix(ex, NX);
-      free_matrix(ey, NX);
-      free_matrix(hz, NX);
-      free(_fict_);
-
-    }else{
-
-      int start, end;
-
-      for(int t = 0; t < tmax; t++){
-
-        if(process_Rank < size_Of_Cluster/2){
-
-          MPI_Recv(&start, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Message Receivid from %d to %d with start %d\n", 0, process_Rank, start);
-
-          MPI_Recv(&end, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Message Receivid from %d to %d with start %d\n", 0, process_Rank, end);
-
-          MPI_Recv(&ey, (end-start) * NX, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Message Receivid from %d to %d with start %f\n", 0, process_Rank, ey[0][0]);
-
-          MPI_Recv(&hz, (end-start) * NX, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Message Receivid from %d to %d with start %f\n", 0, process_Rank, hz[0][0]);
-
-          for (int i=start ; i<end ; i++){
-            for (int j=0 ; j<NY ; j++){
-              ey[i][j] = ey[i][j] - 0.5*(hz[i][j]-hz[i-1][j]);
-            }
-          }
-              
-          MPI_Send(&start, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-          printf("Message Sent from %d to %d with start %d\n", process_Rank, 0, start);
-
-          MPI_Send(&end, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-          printf("Message Sent from %d to %d with end %d\n", process_Rank, 0, end);
-
-          MPI_Send(&ey[start][0], (end-start) * NX, MPI_INT, 0, 1, MPI_COMM_WORLD);
-          printf("Message Sent from %d to %d with end %f\n", process_Rank, 0, ey[start][0]);
-          printf("end - start = %d\n", (end-start)*NX);
-
-          MPI_Send(&hz[start][0], (end-start) * NX, MPI_INT, 0, 1, MPI_COMM_WORLD);
-          printf("Message Sent from %d to %d with end %f\n", process_Rank, 0, hz[start][0]);
-
-          MPI_Barrier(MPI_COMM_WORLD);
-
-
-        }else{
-
-          MPI_Recv(&start, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Message Receivid from %d to %d with start %d\n", 0, process_Rank, start);
-
-          MPI_Recv(&end, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Message Receivid from %d to %d with start %d\n", 0, process_Rank, end);
-
-          MPI_Recv(&ex, (end-start) * NX, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Message Receivid from %d to %d with start %f\n", 0, process_Rank, ex[0][0]);
-
-          MPI_Recv(&hz, (end-start) * NX, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          printf("Message Receivid from %d to %d with start %f\n", 0, process_Rank, hz[0][0]);
-
-          for (int i=0 ; i<NX ; i++){
-            for (int j= start ; end ; j++){
-              ex[i][j] = ex[i][j] - 0.5*(hz[i][j]-hz[i][j-1]);
-            }
-          }
-
-          
-          MPI_Send(&start, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-          printf("Message Sent from %d to %d with start %d\n", process_Rank, 0, start);
-
-          MPI_Send(&end, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-          printf("Message Sent from %d to %d with end %d\n", process_Rank, 0, end);
-
-          MPI_Send(&ex[start][0], (end-start) * NX, MPI_INT, 0, 1, MPI_COMM_WORLD);
-          printf("Message Sent from %d to %d with end %f\n", process_Rank, 0, ex[start][0]);
-          printf("end - start = %d\n", (end-start)*NX);
-
-          MPI_Send(&hz[start][0], (end-start) * NX, MPI_INT, 0, 1, MPI_COMM_WORLD);
-          printf("Message Sent from %d to %d with end %f\n", process_Rank, 0, hz[start][0]);
-
-
-          MPI_Barrier(MPI_COMM_WORLD);
-          
-        }
-
-        
-
-      }
+     
 
     }
 
-    
+    double *scattered_Data_ey = (double*)malloc(NX * (NY/size_Of_Cluster) * sizeof(double));
+
+    int start = 0;
+    int end = NY/size_Of_Cluster;
+
+    // Envia os dados de Ey para os processos com excessão dos dados que o processo 0 vai usar
+    printf("processo 0 enviando %f para todos os processos com start %d e end %d\n", ey[end], start, end);
+    MPI_Scatter(ey, (end-start)*NY, MPI_DOUBLE, scattered_Data_ey, (end-start)*NY, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    //CONTA
+
+    // for (int f = 0; f < NY; f++)
+    //   ey[0][f] = _fict_[t];
+
+    // for (int i=start ; i<end ; i++){
+    //   for (int j=0 ; j<NY ; j++){
+    //     ey[i][j] = ey[i][j] - 0.5*(hz[i][j]-hz[i-1][j]);
+    //   }
+    // }
+
+    // for (int i=start ; i<end ; i++){
+    //   for (int j=0 ; j<NY ; j++){
+    //     ey[i][j] = ey[i][j] - 0.5*(hz[i][j]-hz[i-1][j]);
+    //   }
+    // }
+
+    // for (int i=0 ; i<NX ; i++){
+    //   for (int j= start ; end ; j++){
+    //     ex[i][j] = ex[i][j] - 0.5*(hz[i][j]-hz[i][j-1]);
+    //   }
+    // }
+
+    MPI_Gather(scattered_Data_ey, (end-start)*NY, MPI_DOUBLE, ey, (end-start)*NY, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+    // // free matrixes and array
+    // free_matrix(ex, NX);
+    // free_matrix(ey, NX);
+    // free_matrix(hz, NX);
+    // free(_fict_);
+
     MPI_Finalize();
     return 0;
 }
